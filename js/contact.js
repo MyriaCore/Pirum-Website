6d334bf0963af08770f6604e1cc11b888d2bceed
rfmullens <rmullens@stevens.edu>
2018-11-29 17:44:48 -0500

    plz fix

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,8 @@
 
 window.onload = () => {
     var sub = document.getElementById("submitButton");
-    sub.addEventListener("click", function(){
+    sub.addEventListener("click", function(event){
+        event.preventDefault();
         var name = document.forms["myForm"]["Fname"].value;
         var email = document.forms["myForm"]["email"].value
         var phone = document.forms["myForm"]["phone"].value
@@ -10,49 +11,22 @@ window.onload = () => {
     
         if (name == ""){
             alert("Please fill out Name"); 
-            return false;
         }
         else if (email == ""){
             alert("Please fill out Your Email"); 
-            return false;
         }
         else if (phone == ""){
             alert("Please include your Phone Number"); 
-            return false;
         }
         else if (card == ""){
             alert("Please include your Credit Card"); 
-            return false;
         }
         else if (ssn == ""){
             alert("Please include your Social Security Number for Validation Purposes ;)"); 
-            return false;
         }
         else {
-            function formData(){
-                var credit = document.forms["myForm"]["cardnumber"].value
-                var ss = document.forms["myForm"]["social security"].value
-                console.log(credit);
-                console.log(ss);
-            
-                var div = document.createElement("div");
-            
-                var strcred = document.createTextNode("Your Credit Card Number is" + credit);
-                var ssid = document.createTextNode("Social Security Number is" + ss);
-                var gen = document.createTextNode("Thanks for Your Generosity!!!");
-            
-                div.appendChild(strcred);
-                div.appendChild(ssid);
-                div.appendChild(gen);
-            
-                var formdiv = document.getElementsById("formdiv");
-                div.insertBefore(formdiv)
-            
-                document.getElementsById("formdiv").style.visibility = "hidden";
-            
-            
-            }
             formData();
+            console.log("not broke");
         }
     
        
@@ -61,5 +35,23 @@ window.onload = () => {
 
 
 
+function formData(){
+    var credit = document.forms["myForm"]["cardnumber"].value
+    var ss = document.forms["myForm"]["socialsecurity"].value
+    console.log(credit);
+    console.log(ss);
 
+    var div = document.createElement("div");
+
+    var strcred = document.createTextNode("Your Credit Card Number is" + credit);
+    var ssid = document.createTextNode("Social Security Number is" + ss);
+    var gen = document.createTextNode("Thanks for Your Generosity!!!");
+    
+    var formdiv = document.getElementById("formdiv");
+    
+    formdiv.innerHTML = strcred;
+    
+
+
+}
 
